45cc0f48d51afec74881e88b73cb8ab130a7e9c6
dmitriym <dmitriym.09.12.1989@gmail.com>
2020-04-27 22:36:25 +0300

    add LICENSE

--- setup.py
+++ setup.py
@@ -6,17 +6,23 @@ import setuptools
 
 __verison__ = "1.0.0"
 
-with open('README.md', 'r') as readme_file:
-    long_description = readme_file.read()
-
 setuptools.setup(
     name="py-clamav",
     version=__verison__,
+    license='MIT',
     author="dmitriym09",
+    author_email='dmitriym.09.12.1989@gmail.com',
     description="LibClamAV ctypes binding",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    keywords=['antivirus', 'ClamAv', 'LibClamAV'],
     url="https://github.com/dmitriym09/py-clamav",
     packages=setuptools.find_packages(exclude="test"),
-    python_requires='>=3.8'
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
