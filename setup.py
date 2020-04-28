5e0bc8942c047624e6d874dace60901ac72b4372
dmitriym <dmitriym.09.12.1989@gmail.com>
2020-04-28 22:19:10 +0300

    fix wheel

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ import setuptools
 
 __verison__ = "1.0.0"
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="py-clamav",
     version=__verison__,
@@ -15,14 +18,16 @@ setuptools.setup(
     description="LibClamAV ctypes binding",
     keywords=['antivirus', 'ClamAv', 'LibClamAV'],
     url="https://github.com/dmitriym09/py-clamav",
-    packages=setuptools.find_packages(exclude="test"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src", exclude=["tests", "tests.*"]),
+    package_dir={"": "src"},
     python_requires='>=3.8',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Operating System :: POSIX :: Linux'
     ],
 )
